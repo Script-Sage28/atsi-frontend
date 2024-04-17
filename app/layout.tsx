55a1103faab7bf963eb7c5f4664d0ebe1b39b249
anG0D3v <angod3v@gmail.com>
2024-04-17 18:36:34 +0800

    facebook test

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
     <Fragment>
       <html lang="en">
         <Facebook />
+        <a href="https://auxytech.com/" referrerPolicy="origin"></a>
         <link rel="icon" type="image/png" href={`/logo.png `} />
         <body className="bg-white text-black">
           <ConfigProvider theme={appTheme}>
