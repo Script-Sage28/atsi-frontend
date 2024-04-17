2d59c3e89b728791217a27e9b55af2f3e61141d9
anG0D3v <angod3v@gmail.com>
2024-04-17 18:08:24 +0800

    facebook test

--- app/_document.tsx
+++ app/_document.tsx
@@ -57,17 +57,18 @@ function Facebook() {
             window.fbAsyncInit = function() {
               FB.init({
                 xfbml            : true,
-                version          : 'v13.0'
+                version          : 'v17.0'
               });
             };
       
             (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+            fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));
+            
         `,
         }}
       ></script>
