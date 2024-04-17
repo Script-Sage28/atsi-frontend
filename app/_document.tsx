ab02170ca7bf60f7b539ef5ba11457a3c9f7893f
anG0D3v <angod3v@gmail.com>
2024-04-17 17:44:29 +0800

    facebook test

--- app/_document.tsx
+++ app/_document.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/inline-script-id */
-import Script from 'next/script';
+// import Script from 'next/script';
 
 function Facebook() {
   return (
@@ -7,7 +7,7 @@ function Facebook() {
       <div id="fb-root"></div>
 
       <div id="fb-customer-chat" className="fb-customerchat"></div>
-      <Script strategy="lazyOnload">
+      {/* <Script strategy="lazyOnload">
         {`
             var chatbox = document.getElementById('fb-customer-chat');
             chatbox.setAttribute("page_id", "271939260034412");
@@ -37,7 +37,40 @@ function Facebook() {
               fjs.parentNode.insertBefore(js, fjs);
             }(document, 'script', 'facebook-jssdk'));
         `}
-      </Script>
+      </Script> */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "271939260034412");
+            chatbox.setAttribute("attribution", "biz_inbox");
+            chatbox.setAttribute('theme_color', '#01224F');
+            chatbox.setAttribute(
+              'logged_in_greeting',
+              'Good day! How may I help you?',
+            );
+            chatbox.setAttribute(
+              'logged_out_greeting',
+              'Good day! How may I help you?',
+            );
+            
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v13.0'
+              });
+            };
+      
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        `,
+        }}
+      ></script>
     </>
   );
 }
