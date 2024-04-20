28d0d498bf2b0dbd20b918eb5b7278ca365b4164
Aulkanic <trazonamichael.pdm@gmail.com>
2024-04-20 08:15:58 +0800

    dsd

--- app/product/[brand]/page.tsx
+++ app/product/[brand]/page.tsx
@@ -218,16 +218,6 @@ const handleSliderChange = (value: number[]) => {
 
     </Swiper>
     <div className='my-4 px-8 mt-16'>
-      <div className='w-full justify-end items-end flex flex-col  mb-2 h-max mt-8'>
-        <p className='w-[80%] text-left text-[20px] mb-2'>Search products:</p>
-      <Search
-          placeholder="Enter product here..."
-          className='bg-white w-[80%] rounded-md h-[50px]'
-          name='name'
-          size='large'
-          onChange={useDebounce(onSetFilter)}
-        />
-      </div>
         <div className='w-full flex flex-col md:flex-row gap-2'>
             {/* Filtering */}
             <div className='flex h-max flex-col gap-4'>
@@ -283,22 +273,32 @@ const handleSliderChange = (value: number[]) => {
 
             {/* List */}
             <div className='w-full'>
-            <div className='w-full flex gap-4 md:gap-12 items-start flex-wrap px-2 md:pr-12 md:pl-8 mb-4'>
-              <div className='flex-1 flex-col md:flex-row flex md:items-center gap-4'>
-                <label className='whitespace-nowrap h-full text-[20px]' htmlFor="">Shop by:</label>
-              <Select
-                style={{ width: '100%',height:'50px' }}
-                size='middle'
-                allowClear
-                placeholder='Shop by Brands'
-                value={selectedBrands || brandParams}
-                onChange={handleBrandChange}
-                options={(shopby.brand?.map((data:T_Brand) => ({
-                  value: data.id,
-                  label: data.name,
-                })) as { value: string; label: string; }[])}
-                optionLabelProp="label" 
-              />
+              <div className='w-full justify-end items-end flex flex-col  mb-2 h-max'>
+                <p className='w-full md:pl-8 text-left text-[20px] mb-2'>Search products:</p>
+              <Search
+                  placeholder="Enter product here..."
+                  className='md:pl-8 bg-white w-full rounded-md h-[50px]'
+                  name='name'
+                  size='large'
+                  onChange={useDebounce(onSetFilter)}
+                />
+              </div>
+            <div className='w-full h-24 flex gap-4 md:gap-12 items-end flex-wrap px-2 md:pr-12 md:pl-8 mb-4'>
+              <div className='flex-1 flex-col flex'>
+                <label className='whitespace-nowrap h-full text-[20px]' htmlFor="">Select a Brand:</label>
+                <Select
+                  style={{ width: '100%',height:'50px' }}
+                  size='middle'
+                  allowClear
+                  placeholder='Shop by Brands'
+                  value={selectedBrands || brandParams}
+                  onChange={handleBrandChange}
+                  options={(shopby.brand?.map((data:T_Brand) => ({
+                    value: data.id,
+                    label: data.name,
+                  })) as { value: string; label: string; }[])}
+                  optionLabelProp="label" 
+                />
               </div>
               <div className='w-full md:w-[450px] flex flex-nowrap gap-4 items-bottom'>
                 <div className='flex-1 flex'>
